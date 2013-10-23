c4aac8ad9f4e71ae5c84556a475816ac952293a5
tuannn <nguyen.ngoc.tuan@framgia.com>
2013-10-23 13:53:05 +0700

    first_version_20131023

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Study1::Application.config.secret_token = '9e63d995593b01bbb39b71be4df6651b7ef4b6598e275348fe92b3469f5625024643474a6e1ac014ba3d3b150ff4471273210b67e9ef8c49b7cf92c9cac8c628'
